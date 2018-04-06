f51f68bd9c574cceb195b3cfd24a5a7f75c7f6fd
ruge23 <ruizgerezr@gmail.com>
2018-04-06 12:34:07 -0300

    add slide's animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -16,7 +17,8 @@ import { MainPage } from '../pages/main/main';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserAnimationsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, trigger, transition, style, state, animate, keyframes } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MainPage } from '../main/main';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+import { elementAttribute } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations:[
+    trigger('bounce', [
+      state('*', style({
+          transform: 'translateX(0)'
+      })),
+      transition('* => rightSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(-65px)',  offset: 0.3}),
+        style({transform: 'translateX(0)',     offset: 1.0})
+      ]))),
+      transition('* => leftSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(65px)',  offset: 0.3}),
+        style({transform: 'translateX(0)',     offset: 1.0})
+      ])))
+    ])
+  ]
 })
 export class HomePage {
 
-  skipMsg: String = "Skip";
+  @ViewChild(Slides) slides: Slides;
+  skipMsg: String = 'Skip';
+  state : String = 'x';
 
   constructor(public navCtrl: NavController) {
 
@@ -16,6 +38,22 @@ export class HomePage {
 
   skip(){
     this.navCtrl.push(MainPage);
+  }
+
+  slideChanged(){
+    if(this.slides.isEnd())
+      this.skipMsg = "Alrigth, I got it";
+  }
+
+  slideMoved() {
+    if (this.slides.getActiveIndex() >= this.slides.getPreviousIndex()) 
+      this.state = 'rightSwipe';
+    else 
+      this.state = 'leftSwipe';
+  }
+
+  animationDone() {
+    this.state = 'x';
   }
 
 }

37a7ba5daa26a50b9cdc6e1251f49646779811d0
ruge23 <ruizgerezr@gmail.com>
2018-04-06 10:57:56 -0300

    add slides an button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MainPage } from '../main/main';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  skipMsg: String = "Skip";
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  skip(){
+    this.navCtrl.push(MainPage);
   }
 
 }
